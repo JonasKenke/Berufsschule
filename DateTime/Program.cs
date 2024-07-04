2ea56b395df7ef06cdf39e60b6991bef7b26e6ac
Jonas <jonas.kenke@gmx.net>
2024-07-04 11:20:36 +0200

    datetime edit

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -1,56 +1,43 @@
-﻿namespace DateTime;
-
-class Program
+﻿namespace DateTime
 {
-    public static void Main(string[] args)
+    class Program
     {
-        DateTime dt = new DateTime();
+        public static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
         
-        Console.WriteLine("Date: {0}.{1}.{2} Time: {3}:{4}:{5}", dt.GetDay, dt.GetMonth, dt.GetYear, dt.GetHour, dt.GetMinute, dt
-            .GetSecond);
+            Console.WriteLine("Date: {0}.{1}.{2} Time: {3}:{4}:{5}", dt.GetDay, dt.GetMonth, dt.GetYear, dt.GetHour, dt.GetMinute, dt.GetSecond);
+        }
     }
-}
 
-interface ITime
-{
-    int GetHour { get; }
-    int GetMinute { get; }
-    int GetSecond { get; }
-}
-
-interface IDate
-{
-    int GetDay { get; }
-    int GetMonth { get; }
-    int GetYear { get; }
-}
-
-class DateTime : IDate, ITime
-{
-    private int hour;
-    private int minute;
-    private int second;
-
-    private int day;
-    private int month;
-    private int year;
-
-    public DateTime()
+    interface ITime
     {
-        System.DateTime dt = System.DateTime.Now;
-        day = dt.Day;
-        month = dt.Month;
-        year = dt.Year;
-        
-        hour = dt.Hour;
-        minute = dt.Minute;
-        second = dt.Second;
+        int GetHour { get; }
+        int GetMinute { get; }
+        int GetSecond { get; }
     }
-    
-    public int GetHour => hour;
-    public int GetMinute => minute;
-    public int GetSecond => second;
-    public int GetDay => day;
-    public int GetMonth => month;
-    public int GetYear => year;
+
+    interface IDate
+    {
+        int GetDay { get; }
+        int GetMonth { get; }
+        int GetYear { get; }
+    }
+
+    class DateTime : IDate, ITime
+    {
+        private System.DateTime dt;
+
+        public DateTime()
+        {
+            dt = System.DateTime.Now;
+        }
+
+        public int GetHour => dt.Hour;
+        public int GetMinute => dt.Minute;
+        public int GetSecond => dt.Second;
+        public int GetDay => dt.Day;
+        public int GetMonth => dt.Month;
+        public int GetYear => dt.Year;
+    }
 }
